b0a8e64b61962ab06c5dac7b465dfdd2943cf95f
DylanBlair <dy.blair@gmail.com>
2022-07-13 18:58:19 -0600

    refactor!: created wrapper to hold trigger prop

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,19 @@
+import { ReactNode } from "react";
 import { ReactionPopover } from "./components/ReactionPopover/ReactionPopover";
+import { PopoverProps } from "./types";
 
-export default ReactionPopover;
+interface RQRProps extends PopoverProps {
+  trigger: ReactNode;
+}
+const RQR = (props: RQRProps) => {
+  const { trigger } = props;
+
+  return (
+    <>
+      {trigger}
+      <ReactionPopover {...props} />
+    </>
+  );
+};
+
+export default RQR;
