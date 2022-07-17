86e65ef0f7affac10d5a8c2d7a099c663e58e9d4
DylanBlair <dy.blair@gmail.com>
2022-07-17 12:52:50 -0600

    feat(example): new showcase showing placement\

---
+++ example/src/components/showcaseComponents/Showcase.tsx
@@ -0,0 +1,10 @@
+import { GridShowcase } from "./gridShowcase/GridShowcase";
+import { ShowcaseContainer } from "./ShowcaseStyles";
+
+export const Showcase = () => {
+  return (
+    <ShowcaseContainer>
+      <GridShowcase />
+    </ShowcaseContainer>
+  );
+};
